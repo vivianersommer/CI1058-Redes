e2583bb4d80a22f15e80bac48e4701a864184dd0
Viviane da Rosa Sommer <viviane.sommer@hitechnologies.com.br>
2022-08-04 16:40:05 -0300

    fenix

--- cliente.c
+++ cliente.c
@@ -1,57 +1,69 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <net/ethernet.h>
-#include <linux/if_packet.h>
-#include <linux/if.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <netinet/in.h>
+#include <malloc.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <net/ethernet.h>
+#include <netdb.h>
+#include <linux/if_arp.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <poll.h>
+#include <dirent.h>
+#include <sys/statvfs.h>
 
 #define TENTATIVAS 50
 
+typedef struct sockaddr_ll endereco;
 
 int main(){
 
     int soquete, i;
-    char *mensagem = (unsigned char *) calloc(32, sizeof(unsigned char));
+    struct pollfd p;
+    char *buf;
+	char *comandoAux, *argumentoAux, *lComandoAux;
+    endereco enderecoSocket;
+    char * cwd;
+
+	comandoAux = (char *) malloc(100);
+	lComandoAux = (char *) malloc(100);
+	argumentoAux = (char *) malloc(100);
     
-    // CRIAR SOCKET ----------------------------------------------------------------------------
-	soquete = socket(AF_PACKET , SOCK_RAW , htons(ETH_P_ALL)) ;
-    if(soquete < 0){
-		printf("Erro ao criar Socket!\n");
-		exit(-1);
-	} else {
-        printf("Socket criado com sucesso!\n");
-    }
-    //------------------------------------------------------------------------------------------
-
-    // ALOCA ESPAÇO DA MENSAGEM ----------------------------------------------------------------
-    if (mensagem == NULL) {
-        printf("Erro ao alocar espaço para a mensagem!\n");
-        printf("Motivo:  %s \n",strerror(errno));
-        exit(-1);
-    } else {
-        printf("Sucesso ao alocar espaço para a mensagem!\n");
-    }
-    //------------------------------------------------------------------------------------------
-
-    // LEITURA DA MENSAGEM ---------------------------------------------------------------------
-    for (i = 0; (recv(soquete, mensagem, 32, 0) == -1) && (i < TENTATIVAS); i++) {
-        printf("Erro ao receber mensagem!\n");
-        printf("Motivo:  %s \n",strerror(errno));
-        exit(-1);
-    } 
-    printf("Sucesso ao receber mensagem!\n");
+	
+    system("ifconfig eth0 promisc");
+    soquete = socket(AF_PACKET , SOCK_RAW , htons(ETH_P_ALL)) ;
+   
+    memset(&enderecoSocket, 0, sizeof(endereco));
+	enderecoSocket.sll_family = AF_PACKET;
+	enderecoSocket.sll_protocol = htons(ETH_P_ALL);
+	enderecoSocket.sll_pkttype = PACKET_OTHERHOST;
+	enderecoSocket.sll_ifindex = 2;
+	enderecoSocket.sll_hatype = ARPHRD_ETHER;
+	enderecoSocket.sll_halen = ETH_ALEN;
+	enderecoSocket.sll_addr[0] = 0x54 ;
+	enderecoSocket.sll_addr[1] = 0x04 ;
+	enderecoSocket.sll_addr[2] = 0xA6 ;
+	enderecoSocket.sll_addr[3] = 0x2C ;
+	enderecoSocket.sll_addr[4] = 0x57 ;
+	enderecoSocket.sll_addr[5] = 0x1F ;
+	enderecoSocket.sll_addr[6] = 0x00 ; 
+	enderecoSocket.sll_addr[7] = 0x00 ;  
     
-    if (i >= TENTATIVAS) {
-        printf("Erro , número máximo de tentativas feitas!\n");
-        exit(-1);
-    }
-    //------------------------------------------------------------------------------------------
+    p.fd = soquete;
+	p.events = POLLIN;
 
-    printf("Mensagem recebida = %s \n", mensagem);
+    buf = getcwd(0, 0);
+	strcat(buf, "$ ");
+	printf("%s", buf);
+    fflush(stdin);
+
+	fgets(lComandoAux, 100, stdin);
+	sscanf(lComandoAux, "%s", comandoAux);
+
+    printf("Mensagem recebida = %s \n", comandoAux);
+
     return 1;
 }
